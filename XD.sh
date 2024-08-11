b746c790094c04aad3c21ec335782a01166c2004
HANTER-XD OFFICIAL <129683518+HANTER-XD-OFFICIAL@users.noreply.github.com>
2024-08-11 09:25:37 +0600

    Xd

--- XD.sh
+++ XD.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
 
+import platform
 import os
-import file_cark
 
-file_cark.login()
+# আর্কিটেকচার চেক করা
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+    print("64-bit")
+    # 64-বিট কোড এখানে থাকবে
+    os.system('python3 BIT_64.py')
+elif bit == '32bit':
+    print("32-bit")
+    # 32-বিট কোড এখানে থাকবে
+    os.system('python3 BIT_32.py')
+else:
+    print("অজানা সিস্টেম আর্কিটেকচার")
