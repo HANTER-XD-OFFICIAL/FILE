400b227d46a9c1c5110b2e0e4ffe170ecc4a198e
HANTER-XD OFFICIAL <129683518+HANTER-XD-OFFICIAL@users.noreply.github.com>
2024-08-11 09:12:26 +0600

    Update XD.sh

--- XD.sh
+++ XD.sh
@@ -1,18 +1,3 @@
-#!/usr/bin/env python3
-
-import platform
 import os
-
-# আর্কিটেকচার চেক করা
-bit = platform.architecture()[0]
-
-if bit == '64bit':
-    print("64-bit")
-    # 64-বিট কোড এখানে থাকবে
-    os.system('python3 BIT_64.py')
-elif bit == '32bit':
-    print("32-bit")
-    # 32-বিট কোড এখানে থাকবে
-    os.system('python3 BIT_32.py')
-else:
-    print("Unknown system architecture")
+import file_cark
+file_cark.login()
